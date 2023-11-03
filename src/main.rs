3df1c4c5ccea35f279ff3dfaaa2c3923bb2f8c3d
patelpratham11 <ppatel011102@gmail.com>
2023-11-03 13:22:25 -0400

    finished passcode gen

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,54 @@
+#[allow(non_snake_case)]
+
 use clap::Parser;
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
 
 /// Simple passphrase generator based on diceware
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Number of words to generate
+    /// Number of words or length to generate
     #[arg(short, long, default_value_t = 5)]
-    words: u8,
+    length: u8,
 
     /// Delimiter character
-    #[arg(short, long, default_value = "-" )]
+    #[arg(short, long, default_value = "-", required = false )]
     delimiter: String,
 
     /// Number of passphrases to generate
-    #[arg(short, long, default_value = 1)]
+    #[arg(short, long, default_value_t = 10)]
     number: u8,
+
+    /// Generate phrases or codes [0: Code !0: Phrases]
+    #[arg(short, long, required = true)]
+    phrase: u8,
 }
 
 fn main() {
     let args = Args::parse();
+    
+    if args.phrase == 0 {
+        for _ in 0..args.number{
+            let rand_string: String = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(40)
+            .map(char::from)
+            .collect();
+            println!("{}\n\n\tEntropy: {}", rand_string, entropy(&rand_string));
+        }
+    } else{
+        println!("Implement phrases");
+    }
+    
+}
 
-    println!("{} words with {} delim", args.words, args.delimiter);
+fn entropy(pass: &str) -> f32 {
+    (pass.chars().count() as f32)*(calculate_unique(pass).log2())
+}
+
+fn calculate_unique(pass: &str) -> f32{
+    let mut v: Vec<char> = pass.chars().collect();
+    v.dedup();
+    v.len() as f32
 }
